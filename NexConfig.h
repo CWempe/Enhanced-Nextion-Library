5fc38f402574995e8f5a821cb291202966a5ce75
Christoph Wempe <christoph@wempe.net>
2020-11-08 00:11:57 +0100

    Use SoftSerial

--- NexConfig.h
+++ NexConfig.h
@@ -24,7 +24,7 @@
  * Define DEBUG_SERIAL_ENABLE to enable debug serial. 
  * Comment it to disable debug serial. 
  */
-#define DEBUG_SERIAL_ENABLE
+//#define DEBUG_SERIAL_ENABLE
 
 /**
  * Define dbSerial for the output of debug messages. 
@@ -34,7 +34,10 @@
 /**
  * Define nexSerial for communicate with Nextion touch panel. 
  */
-#define nexSerial Serial2
+//#define nexSerial Serial2
+#include <SoftwareSerial.h>
+extern SoftwareSerial HMISerial;
+#define nexSerial HMISerial
 
 
 #ifdef DEBUG_SERIAL_ENABLE
